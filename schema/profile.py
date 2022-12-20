40c0ac860633f1d02ea0cd6552d54fd26c62654f
DHM1996 <dmarkarian@fi.uba.ar>
2022-12-20 13:53:37 -0300

    update backend

--- schema/profile.py
+++ schema/profile.py
@@ -5,7 +5,7 @@ from typing import Optional
 class ProfileSchema(BaseModel):
     user_id: int
     name: Optional[str]
-    link_pic: Optional[float]
+    link_pic: Optional[str]
     longitude: Optional[float]
     latitude: Optional[float]
     profession_id: Optional[int]
