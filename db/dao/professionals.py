1bf56b8c4f8a58d35de0df25e42f3363ad6e0b9b
DHM1996 <dmarkarian@fi.uba.ar>
2022-12-20 14:05:27 -0300

    update backend

--- db/dao/professionals.py
+++ db/dao/professionals.py
@@ -6,7 +6,7 @@ from db.model.users import User
 
 def get_professionals_by_profession(profession_id):
     try:
-        query = db_session.query(User)
+        query = db_session.query(User.name, User.link_pic, User.longitude, User.latitude, User.profession_id)
 
         if profession_id:
             query = query.filter(User.profession_id == profession_id)
@@ -21,8 +21,8 @@ def get_professionals_by_profession(profession_id):
 
 def get_professional_by_id(professional_id):
     try:
-        return db_session.query(User).filter(User.id == professional_id). \
-            filter(User.profession_id != None).all()
+        return db_session.query(User.name, User.link_pic, User.longitude, User.latitude, User.profession_id).filter(
+            User.id == professional_id).filter(User.profession_id != None).all()
     except Exception as err:
         db_session.rollback()
         raise err
@@ -30,7 +30,7 @@ def get_professional_by_id(professional_id):
 
 def get_professional_by_name(professional_name, profession_id=None):
     try:
-        query = db_session.query(User).filter(func.lower(User.name).like(f'%{professional_name.lower()}%'))
+        query = db_session.query(User.name, User.link_pic, User.longitude, User.latitude, User.profession_id).filter(func.lower(User.name).like(f'%{professional_name.lower()}%'))
         if profession_id:
             query = query.filter(User.profession_id == profession_id)
         else:
